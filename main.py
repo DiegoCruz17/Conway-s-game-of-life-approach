c3856ef97b34888e6570300de4403e31bbff3606
Diego <74324920+DiegoCruz17@users.noreply.github.com>
2022-09-10 16:07:59 -0500

    Commented

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from game import Game
 
 pygame.init()
+#The first argument handles the number of cells, while the second handles the frame rate. Lowering the framerate increases performance(?
 game = Game(50,30)
 while True:
     game.getEvents()
